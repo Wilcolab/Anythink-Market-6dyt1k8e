b3ef21046594a450032dddc86038951777a8e569
Subu1023 <settu.oft@gmail.com>
2026-01-16 14:59:04 +0530

    client.js

--- public/client.js
+++ public/client.js
@@ -33,8 +33,8 @@ function calculate(operand1, operand2, operation) {
         case '/':
             uri += "?operation=divide";
             break;
-            case '/':
-            uri += "?operation=divide";
+             case '^':
+            uri += "?operation=power";
             break;
         default:
             setError();
